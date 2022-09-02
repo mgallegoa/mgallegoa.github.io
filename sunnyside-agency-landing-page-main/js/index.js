a3a0966885a4f98ee0efa3c9cb1e273a6219070b
Manuel Fernando Gallego Arias <manuelgallego2003@yahoo.com>
2022-09-01 19:56:22 -0500

    Frontendmentor: sunnyside, mobile menu tuning

---
+++ sunnyside-agency-landing-page-main/js/index.js
@@ -0,0 +1,8 @@
+function showHiddeMenu() {
+    var x = document.getElementById("mobile-menu");
+    if (x.style.display === "block") {
+    x.style.display = "none";
+    } else {
+    x.style.display = "block";
+    }
+}
