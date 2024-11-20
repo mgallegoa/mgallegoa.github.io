1d1aa00646fc0365d6749dfec9f309d7c480d1a2
mgallegoa <manuelgallego2003@yahoo.com>
2024-11-20 13:31:06 -0500

    Node: express movies REST API, solutions for CORS, normal and CORS-PreFlight methods

--- vanillaJS/basic_express_movies/src/app_movies.js
+++ vanillaJS/basic_express_movies/src/app_movies.js
@@ -1,21 +1,48 @@
 const express = require("express");
 const movies = require("./db/movies.json");
 const { validateSchema, validatePartialSchema } = require("./schemas/movie");
+// const cors = require("cors");
 
+// NOTE: for CORS normal methods GET/HEAD/POST, complex methods PUT/PATCH/DELETE (Require OPTIONS request) CORS Pre-flight
 // GET movies or GET movies?genre=Action
 // GET movies/:id
 // POST movies
 // PATCH movies/:id
 // PUT movies/:id
+// DELETE movies/:id
 const app = express();
 
 app.disable("x-powered-by");
 app.use(express.json());
 
+const ACCEPTED_ORIGINS = [
+  "http://localhost:40033",
+  "http://localhost:36665",
+  "http://localhost:8080",
+  "http://urlOfProduction.com",
+];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//         callback(null, true);
+//       }
+//       return callback(new Error(`Origin ${origin} Not Allowed`));
+//     },
+//   }),
+// );
+
 app.get("/", (req, res) => {
   res.end("<h1>Server Running Manuel</h1>");
 });
 app.get("/movies", (req, res) => {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:40033");
+  const origin = req.header("origin");
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   const { genre } = req.query;
   if (genre) {
     const filterMovies = movies.filter((movie) =>
@@ -69,14 +96,27 @@ app.patch("/movies/:id", (req, res) => {
 });
 
 app.delete("/movies/:id", (req, res) => {
+  const origin = req.header("origin");
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   const { id } = req.params;
   const movieIndex = movies.findIndex((movie) => movie.id === id);
 
   if (movieIndex === -1) {
     return res.status(404).json(`Movie to DELETE with id: ${id}, not found`);
   }
-  movies.splice(movieIndex);
+  movies.splice(movieIndex, 1);
   return res.status(200).json(`Movie with id: ${id} was delete`);
+});
+
+app.options("/movies/:id", (req, res) => {
+  const origin = req.header("origin");
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  }
+  res.send(200);
 });
 
 const port = process.env.PORT ?? 3000;
