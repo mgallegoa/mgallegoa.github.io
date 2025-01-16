4f2c3be5f2cdb0a42f0e5ae4c36f669b33809cac
mgallegoa <manuelgallego2003@yahoo.com>
2025-01-15 21:35:15 -0500

    JWT : add logic for the jsonwebtoken and manage the cookies.

--- vanillaJS/basic_express_jwt/config.js
+++ vanillaJS/basic_express_jwt/config.js
@@ -1,1 +1,5 @@
-export const { PORT = 3000, SALT_ROUNDS = 10 } = process.env;
+export const {
+  PORT = 3000,
+  SALT_ROUNDS = 10,
+  SECRET_JWT_KEY = "Super long and secret for this example this is a default value",
+} = process.env;
--- vanillaJS/basic_express_jwt/index.js
+++ vanillaJS/basic_express_jwt/index.js
@@ -1,13 +1,29 @@
 import express, { json } from "express";
-import { PORT } from "./config.js";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import { PORT, SECRET_JWT_KEY } from "./config.js";
 import { UserRepository } from "./user-repository.js";
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  const token = req.cookies.manuel_access_token;
+  req.session = { user: null };
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY);
+    req.session.user = data;
+  } catch (error) {
+    console.log(error);
+  }
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.render("index", { username: "Arias" });
+  const { user } = req.session;
+  res.render("index", user);
 });
 
 app.post("/register", async (req, res) => {
@@ -29,7 +45,22 @@ app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await UserRepository.login({ username, password });
-    res.send(user);
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      SECRET_JWT_KEY,
+      {
+        // Access token short, Refresh token long
+        expiresIn: "1h",
+      },
+    );
+    res
+      .cookie("manuel_access_token", token, {
+        httpOnly: true, // The cookie is only access in the server
+        secure: process.env.NODE_ENV === "production", // Access the cookie only with https
+        sameSite: "strict", // The cookie only accessed in the same domain. Not work document.cookie
+        maxAge: 1000 * 60 * 60, // The cookie only valid in 1 hour
+      })
+      .send(user);
   } catch (error) {
     console.log(error);
     res.status(400);
@@ -38,10 +69,14 @@ app.post("/login", async (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.end({ id: "hi" });
+  res.clearCookie("manuel_access_token").json({ message: "Logout sussefull" });
 });
 app.get("/protected", (req, res) => {
-  res.render("protected", { username: "Arias" });
+  const { user } = req.session;
+  if (!user) {
+    return res.status(403).send("Access not authorized.");
+  }
+  res.render("protected", user);
 });
 
 app.listen(PORT, () => {
