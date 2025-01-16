2818f6038c5727df1278ea88cfbfcab5665c8b04
mgallegoa <manuelgallego2003@yahoo.com>
2025-01-16 16:01:10 -0500

    CHAT : Add and serve the client side in html, add morgan logs.

--- vanillaJS/basic_chat_sockets/server/index.js
+++ vanillaJS/basic_chat_sockets/server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import logger from "morgan";
 
 const app = express();
 
 const port = process.env.PORT ?? 3000;
 
+app.use(logger("dev"));
 app.get("/", (req, res) => {
-  res.send("Hi from server");
+  res.sendFile(process.cwd() + "/client/index.html");
 });
 
 app.listen(port, () => {
