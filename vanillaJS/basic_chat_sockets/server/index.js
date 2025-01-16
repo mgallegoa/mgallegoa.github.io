6e884129269f6ece1700807be2e52e67887e91eb
mgallegoa <manuelgallego2003@yahoo.com>
2025-01-16 17:31:17 -0500

    CHAT : Add socked.io library and add logic for chat working in the html.

--- vanillaJS/basic_chat_sockets/server/index.js
+++ vanillaJS/basic_chat_sockets/server/index.js
@@ -1,7 +1,21 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("user connection");
+  socket.on("disconnect", () => {
+    console.log("Disconnected.");
+  });
+  socket.on("chat_message", (message) => {
+    io.emit("chat_message", message);
+  });
+});
 
 const port = process.env.PORT ?? 3000;
 
@@ -10,6 +24,6 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening in port ${port}`);
 });
